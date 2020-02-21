0efd60c3d9b5ef44a0176425beb841c1820abe57
Marco Silvestroni <silvestronimarco@gmail.com>
2020-02-21 12:50:50 +0100

    fix w/h add default image for folder

--- upload/catalog/controller/extension/module/banner_pro.php
+++ upload/catalog/controller/extension/module/banner_pro.php
@@ -4,10 +4,10 @@ class ControllerExtensionModuleBannerPro extends Controller
         public function index($setting)
         {
                 static $module = 0;
-                
+
                 $this->load->language('extension/module/banner_pro');
                 $this->load->model('extension/module/banner_pro');
-                $this->load->model('tool/image');
+                //$this->load->model('tool/image');
 
                 $this->document->addStyle('catalog/view/javascript/jquery/swiper/css/swiper.min.css');
                 $this->document->addStyle('catalog/view/javascript/jquery/swiper/css/opencart.css');
@@ -16,25 +16,25 @@ class ControllerExtensionModuleBannerPro extends Controller
                 $data['banners'] = array();
 
                 $results = $this->model_extension_module_banner_pro->getBanner($setting['banner_id']);
-
+                $host = HTTPS_SERVER ? HTTPS_SERVER : HTTP_SERVER;
                 foreach ($results as $result) {
                         /*   if (is_file(DIR_IMAGE . $result['image'])) { */
-                                  $data['banners'][] = array(
-                                          'title'      => $result['title'],
-                                          'subtitle'      => $result['subtitle'],
-                                          'description'      => $result['description'],
-                                          'link1'       => $result['link1'],
-                                          'label1'      => $result['label1'],
-                                          'link2'      => $result['link2'],
-                                          'label2'       => $result['label2'],
-                                          'video'      => $result['video'],
-                                          'image' => $this->model_tool_image->resize($result['image'], $setting['width'], $setting['height'])
-                                  );
-                       /*    } */
-                  }
+                        $data['banners'][] = array(
+                                'title'      => $result['title'],
+                                'subtitle'      => $result['subtitle'],
+                                'description'      => $result['description'],
+                                'link1'       => $result['link1'],
+                                'label1'      => $result['label1'],
+                                'link2'      => $result['link2'],
+                                'label2'       => $result['label2'],
+                                'video'      => $result['video'],
+                                'image' =>  $host.'image/'.$result['image']
+                        );
+                        /*    } */
+                }
 
                 $data['module'] = $module++;
 
-                return $this->load->view("extension/module/".$setting['layout'], $data);
+                return $this->load->view("extension/module/" . $setting['layout'], $data);
         }
 }
